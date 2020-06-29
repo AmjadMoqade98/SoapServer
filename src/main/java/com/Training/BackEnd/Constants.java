acedd7d925650ca2e12e84777bdf5ed44531b372
AmjadMoqade98 <Amjad.Moqade@exalt.email>
2020-06-29 16:28:24 +0300

    no message

--- src/main/java/com/Training/BackEnd/Constants.java
+++ src/main/java/com/Training/BackEnd/Constants.java
@@ -3,6 +3,6 @@ package com.Training.BackEnd;
 public final class Constants {
 
     public static final String  SoapNamespace = "http://amjad.com/bundles" ;
-    public static final String SoapFile = "bundles.xsd" ;\
+    public static final String SoapFile = "bundles.xsd" ;
     //amkjad
 }
